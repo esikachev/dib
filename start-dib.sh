b3be8c2a317ad490deea912368da11f50328c5c9
Evgeny Sikachev <esikachev@mirantis.com>
2015-08-24 17:28:38 +0300

    Restructure project

--- start-dib.sh
+++ start-dib.sh
@@ -5,17 +5,16 @@
 #popd sahara-image-elements
 
 SCREEN_NAME=dib
-IMAGES=images.conf
 
 screen -d -m -S $SCREEN_NAME -t shell -s /bin/bash
 sleep 1
 
-for image in $IMAGES
-do
-    plugin=`awk -F'\t' {'print $2'} $image`
-    version=`awk -F'\t' {'print $2'} $image`
-    os=`awk -F'\t' {'print $2'} $image`
-    #screen -X $SCREEN_NAME -t
-    echo $plugin
-    sleep 0.1
+plugins=( cloudera vanilla spark mapr )
+versions=( 5.4.0 2.6.0 1.3.1 4.0.2 )
+
+for n in {0..3}; do
+  # create now window using `screen` command
+  name=${plugins[n]}
+  screen -S $SCREEN_NAME -X screen $name
+  screen -S $SCREEN_NAME -p $name -X stuff tox -e venv -- sahara-image-create -p $name -v ${version[n]}
 done
